a05c24373bc9322dc412a5e93feaa54715e1e345
hinakilam <hina.malik@kellogg.northwestern.edu>
2022-02-16 01:16:58 +0000

    hw

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,8 @@
 class User < ApplicationRecord
+    def balance
+        coins_received = Transaction.where({ to_user_id: self.id }).sum(:amount)
+        coins_given = Transaction.where({from_user_id: self.id}).sum(:amount)
+        coins_received - coins_given
+    end 
 end
+
